7129c828823bae99094459c056262c24815b8b63
Ricky <ricky@dius.com.au>
2014-07-30 21:05:03 +1000

    more mods

--- CardFlip/ViewController.swift
+++ CardFlip/ViewController.swift
@@ -11,36 +11,37 @@ import UIKit
 class ViewController: UIViewController {
     
     var cardView: UIView!
-    var backImageView: UIImageView!
-    var cardImageView: UIImageView!
+    var back: UIImageView!
+    var front: UIImageView!
     
     var showingBack = true
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        cardImageView = UIImageView(image: UIImage(named: "c01.png"))
-        backImageView = UIImageView(image: UIImage(named: "Card-Back-01.png"))
-
+        front = UIImageView(image: UIImage(named: "front.png"))
+        back = UIImageView(image: UIImage(named: "back.png"))
+        
         let singleTap = UITapGestureRecognizer(target: self, action: Selector("tapped"))
         singleTap.numberOfTapsRequired = 1
-
-        let rect = CGRectMake(20, 20, backImageView.image.size.width, backImageView.image.size.height)
+        
+        let rect = CGRectMake(20, 20, back.image.size.width, back.image.size.height)
         cardView = UIView(frame: rect)
         cardView.addGestureRecognizer(singleTap)
         cardView.userInteractionEnabled = true
-        cardView.addSubview(backImageView)
-        cardView.sizeToFit()
-    
+        cardView.addSubview(back)
+        
         view.addSubview(cardView)
     }
 
+
     func tapped() {
         if (showingBack) {
-            UIView.transitionFromView(backImageView, toView: cardImageView, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+            UIView.transitionFromView(back, toView: front, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
             showingBack = false
         } else {
-            UIView.transitionFromView(cardImageView, toView: backImageView, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+            UIView.transitionFromView(front, toView: back, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
             showingBack = true
         }
 
