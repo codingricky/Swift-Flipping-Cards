d8e62d038576813333183734f602025095f0dd22
Suryakant <suryakant.sharma@fatbit.in>
2018-08-27 12:01:47 +0530

    -   Convert Code to Swift 4 Xcode 9.4.1 -   Minor Optimization

--- CardFlip/ViewController.swift
+++ CardFlip/ViewController.swift
@@ -16,32 +16,31 @@ class ViewController: UIViewController {
     
     var showingBack = true
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         front = UIImageView(image: UIImage(named: "front.png"))
         back = UIImageView(image: UIImage(named: "back.png"))
         
-        let singleTap = UITapGestureRecognizer(target: self, action: Selector("tapped"))
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(tapped))
         singleTap.numberOfTapsRequired = 1
         
-        let rect = CGRectMake(20, 20, back.image.size.width, back.image.size.height)
+        let rect = CGRect(x: 20,y: 20, width: back.image!.size.width , height: back.image!.size.height)
         cardView = UIView(frame: rect)
         cardView.addGestureRecognizer(singleTap)
-        cardView.userInteractionEnabled = true
+        cardView.isUserInteractionEnabled = true
         cardView.addSubview(back)
         
         view.addSubview(cardView)
     }
 
 
-    func tapped() {
-        if (showingBack) {
-            UIView.transitionFromView(back, toView: front, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+    @objc func tapped() {
+        if showingBack {
+            UIView.transition(from: back, to: front, duration: 1, options: UIViewAnimationOptions.transitionFlipFromLeft, completion: nil)
             showingBack = false
         } else {
-            UIView.transitionFromView(front, toView: back, duration: 1, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+            UIView.transition(from: front, to: back, duration: 1, options: UIViewAnimationOptions.transitionFlipFromLeft, completion: nil)
             showingBack = true
         }
 
